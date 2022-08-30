db633ece45292682924674920e1e07f886fe7f3b
HAMAM MUBAROK <111847088+HAMZz14@users.noreply.github.com>
2022-08-30 21:07:09 +0700

    Add files via upload

---
+++ contoh2.php
@@ -0,0 +1,13 @@
+<?php
+	function jumlah($banyak, $nama, $umur)
+	{
+		for ($i = 0; $i < $banyak; $i++)
+		{
+			echo(" Nama saya ".$nama." umur ".$umur." tahun"."<br>");
+		}
+			echo(" "."<br>");
+	}
+	jumlah(3,'Hamam',14);
+	jumlah(3,'Hamim',29);
+	jumlah(3,'Hakim',27);
+?>
